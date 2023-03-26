6c1720d894177b6cd8c9a599e463a093a441fa88
sanghyeok-kim <slsnsi1212@gmail.com>
2023-03-26 23:30:50 +0900

    feat: MVVM 구조화를 위한 ViewModelType, ViewType 프로토콜 생성 및 구현

---
+++ MyGitHubTracker/MyGitHubTracker/Source/Presentation/Support/Protocol/MVVMInterface.swift
@@ -0,0 +1,33 @@
+//
+//  MVVMInterface.swift
+//  MyGitHubTracker
+//
+//  Created by 김상혁 on 2023/03/26.
+//
+
+import Foundation
+
+protocol ViewModelType: AnyObject {
+    associatedtype Input
+    associatedtype Output
+
+    var input: Input { get }
+    var output: Output { get }
+}
+
+protocol ViewType: AnyObject {
+    associatedtype ViewModel: ViewModelType
+    
+    var viewModel: ViewModel? { get set }
+    
+    func bindInput(to viewModel: ViewModel)
+    func bindOutput(from viewModel: ViewModel)
+}
+
+extension ViewType {
+    func bind(viewModel: ViewModel) {
+        self.viewModel = viewModel
+        bindInput(to: viewModel)
+        bindOutput(from: viewModel)
+    }
+}
