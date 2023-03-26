53df48a09d4b398fadf99f72dec8e7e04c67fccd
sanghyeok-kim <slsnsi1212@gmail.com>
2023-03-26 23:18:39 +0900

    init: 프로젝트 파일 생성

---
+++ MyGitHubTracker/MyGitHubTrackerUITests/MyGitHubTrackerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MyGitHubTrackerUITestsLaunchTests.swift
+//  MyGitHubTrackerUITests
+//
+//  Created by 김상혁 on 2023/03/26.
+//
+
+import XCTest
+
+class MyGitHubTrackerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
