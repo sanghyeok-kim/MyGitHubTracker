8295aab4f9e9058cd132c206a005e0ac2c02fa21
sanghyeok-kim <slsnsi1212@gmail.com>
2023-03-27 10:48:22 +0900

    feat: AccessToken 얻기 위한 callback URL 반환하는 GitHubAuthorizationService 구현

---
+++ MyGitHubTracker/MyGitHubTracker/Source/Data/Service/DefaultGitHubAuthorizationService.swift
@@ -0,0 +1,29 @@
+//
+//  DefaultGitHubAuthorizationService.swift
+//  MyGitHubTracker
+//
+//  Created by 김상혁 on 2023/03/27.
+//
+
+import Foundation
+
+protocol GitHubAuthorizationService {
+    func buildGitHubAuthorizationURL() -> URL?
+}
+
+final class DefaultGitHubAuthorizationService: GitHubAuthorizationService {
+    func buildGitHubAuthorizationURL() -> URL? {
+        let target = GitHubAPI.fetchTempCode
+        
+        guard let url = target.baseURL?.appendingPathComponent(target.path) else { return nil }
+        var urlComponents = URLComponents(string: url.absoluteString)
+
+        if let parameters = target.parameters {
+            urlComponents?.queryItems = parameters.map {
+                URLQueryItem(name: $0.key, value: String(describing: $0.value))
+            }
+        }
+
+        return urlComponents?.url
+    }
+}
