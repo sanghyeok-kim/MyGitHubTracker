0831393b065d0df127c063dd6033b4d562688bdd
sanghyeok-kim <slsnsi1212@gmail.com>
2023-03-27 10:30:07 +0900

    feat: 상수값 관리하는 Constant 생성, AccesTokne과 GitHubClientKey에 접근하는 Key 상수 생성

---
+++ MyGitHubTracker/MyGitHubTracker/Source/Common/Constant.swift
@@ -0,0 +1,27 @@
+//
+//  Constant.swift
+//  MyGitHubTracker
+//
+//  Created by 김상혁 on 2023/03/27.
+//
+
+import Foundation
+
+enum Constant {
+    
+}
+
+extension Constant {
+    enum GitHubAPI {
+        static let acceseToken = "AccessToken"
+    }
+}
+
+extension Constant {
+    enum GitHubClientKey {
+        static let fileExtension = "plist"
+        static let fileName = "GitHubClientKey"
+        static let clientID = Bundle.main.object(forGitHubClientKeyDictionaryKey: "ClientID") as? String ?? ""
+        static let clientSecret = Bundle.main.object(forGitHubClientKeyDictionaryKey: "ClientSecret") as? String ?? ""
+    }
+}
