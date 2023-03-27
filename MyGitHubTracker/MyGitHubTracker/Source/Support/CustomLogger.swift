7856596e64602fc6946abea6e79124e5f33fa649
sanghyeok-kim <slsnsi1212@gmail.com>
2023-03-28 08:13:56 +0900

    feat: CustomLogger 생성 및 구현
    - message, category, type 을 매개변수로 받아서 콘솔에 log를 출력

---
+++ MyGitHubTracker/MyGitHubTracker/Source/Support/CustomLogger.swift
@@ -0,0 +1,15 @@
+//
+//  CustomLogger.swift
+//  MyGitHubTracker
+//
+//  Created by 김상혁 on 2023/03/28.
+//
+
+import OSLog
+
+struct CustomLogger {
+    static func log(message: String, category: OSLog.LogCategory, type: OSLogType = .default) {
+        let log = OSLog(subsystem: OSLog.subsystem, category: category.value)
+        os_log(type, log: log, "\(message)")
+    }
+}
