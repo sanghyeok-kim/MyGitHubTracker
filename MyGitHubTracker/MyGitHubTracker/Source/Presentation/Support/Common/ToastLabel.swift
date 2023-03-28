7dfaec7a7ce01eb6d23cfd33a2e15b25c7db2332
sanghyeok-kim <slsnsi1212@gmail.com>
2023-03-28 23:38:10 +0900

    refactor: ToastLabel이 RoundedPaddingLabel을 상속받는 구조로 변경

--- MyGitHubTracker/MyGitHubTracker/Source/Presentation/Support/Common/ToastLabel.swift
+++ MyGitHubTracker/MyGitHubTracker/Source/Presentation/Support/Common/ToastLabel.swift
@@ -7,40 +7,23 @@
 
 import UIKit
 
-final class ToastLabel: UILabel {
-    
-    private var padding: UIEdgeInsets
-    
-    override var intrinsicContentSize: CGSize {
-        var contentSize = super.intrinsicContentSize
-        if contentSize == .zero { return contentSize }
-        contentSize.height += padding.top + padding.bottom
-        contentSize.width += padding.left + padding.right
-        return contentSize
-    }
-    
-    init(padding: UIEdgeInsets = UIEdgeInsets(top: 8, left: 16, bottom: 8, right: 16)) {
-        self.padding = padding
-        super.init(frame: .zero)
+final class ToastLabel: RoundedPaddingLabel {
+    override init(padding: Padding = .medium) {
+        super.init(padding: padding)
         configureUI()
     }
     
-    @available(*, unavailable)
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    func show(message: String) {
+        text = message
+        sizeToFit()
+        fadeIn(completion: delayedFadeOut)
     }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        clipsToBounds = true
-        layer.cornerRadius = min(frame.width, frame.height) / 2
-    }
-    
-    override func drawText(in rect: CGRect) {
-        super.drawText(in: rect.inset(by: padding))
-    }
-    
-    private func configureUI() {
+}
+
+// MARK: - Interval Methods
+
+private extension ToastLabel {
+    func configureUI() {
         alpha = .zero
         font = UIFont.systemFont(ofSize: 15)
         textAlignment = .center;
@@ -50,13 +33,7 @@ final class ToastLabel: UILabel {
         lineBreakMode = .byWordWrapping
     }
     
-    func show(message: String) {
-        text = message
-        sizeToFit()
-        fadeIn(completion: delayedFadeOut)
-    }
-    
-    private func fadeIn(completion: @escaping () -> Void) {
+    func fadeIn(completion: @escaping () -> Void) {
         UIView.animate(withDuration: 0.5, animations: { [weak self] in
             self?.alpha = 1.0
         }, completion: { _ in
@@ -64,7 +41,7 @@ final class ToastLabel: UILabel {
         })
     }
     
-    private func delayedFadeOut() {
+    func delayedFadeOut() {
         UIView.animate(withDuration: 0.5, delay: 3.0, options: .curveEaseIn, animations: { [weak self] in
             self?.alpha = .zero
         }, completion: { [weak self] _ in
