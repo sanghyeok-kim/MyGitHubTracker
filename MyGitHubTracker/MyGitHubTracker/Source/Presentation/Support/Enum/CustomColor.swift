2534e1507c917514f9f86a52ff10e7332e6792b5
sanghyeok-kim <slsnsi1212@gmail.com>
2023-03-29 10:31:40 +0900

    feat: CustomColor 열거형 구현

---
+++ MyGitHubTracker/MyGitHubTracker/Source/Presentation/Support/Enum/CustomColor.swift
@@ -0,0 +1,13 @@
+//
+//  CustomColor.swift
+//  MyGitHubTracker
+//
+//  Created by 김상혁 on 2023/03/28.
+//
+
+import UIKit.UIColor
+
+enum CustomColor {
+    static let softRed = UIColor(red: 1.0, green: 0.5, blue: 0.5, alpha: 1.0)
+    static let softGreen = UIColor(red: 0.5, green: 0.8, blue: 0.5, alpha: 1.0)
+}
