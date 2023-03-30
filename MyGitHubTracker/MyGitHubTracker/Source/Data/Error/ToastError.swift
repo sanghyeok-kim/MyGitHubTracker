38a312cc0e95b0859769240288005e6991d62778
sanghyeok-kim <slsnsi1212@gmail.com>
2023-03-30 12:53:35 +0900

    feat: ToastError에 새로운 case 추가

--- MyGitHubTracker/MyGitHubTracker/Source/Data/Error/ToastError.swift
+++ MyGitHubTracker/MyGitHubTracker/Source/Data/Error/ToastError.swift
@@ -7,13 +7,19 @@
 
 import Foundation
 
-enum ToastError: Error, LocalizedError {
+enum ToastError: LocalizedError {
     case failToFetchAccessToken
+    case failToFetchRepositories
+    case failToFetchUserInformation
     
     var errorDescription: String? {
         switch self {
         case .failToFetchAccessToken:
-            return "사용자 인증 토큰을 불러오는데 실패했습니다."
+            return "사용자 인증 토큰을 불러오는 데 실패했습니다."
+        case .failToFetchRepositories:
+            return "사용자 레포지토리를 불러오는 데 실패했습니다."
+        case .failToFetchUserInformation:
+            return "사용자 정보를 불러오는 데 실패했습니다."
         }
     }
 }
