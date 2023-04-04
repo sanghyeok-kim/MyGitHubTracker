e3dc2b569f3ba8a622751306069b1d3a61ef7859
sanghyeok-kim <slsnsi1212@gmail.com>
2023-04-04 22:41:58 +0900

    feat: RepositoryDTO에 owner, stargazersCount 프로퍼티 추가

--- MyGitHubTracker/MyGitHubTracker/Source/Data/DTO/RepositoryDTO.swift
+++ MyGitHubTracker/MyGitHubTracker/Source/Data/DTO/RepositoryDTO.swift
@@ -10,13 +10,23 @@ import Foundation
 struct RepositoryDTO: Decodable {
     let name: String
     let `private`: Bool
+    let owner: OwnerDTO
     let description: String?
     let updatedAt: String
+    let stargazersCount: Int
     
     enum CodingKeys: String, CodingKey {
         case name
         case `private`
+        case owner
         case description
         case updatedAt = "updated_at"
+        case stargazersCount = "stargazers_count"
+    }
+}
+
+extension RepositoryDTO {
+    struct OwnerDTO: Decodable {
+        let login: String
     }
 }
