90af4639040df98c2f78c5f61dcbceb736ad3158
sanghyeok-kim <slsnsi1212@gmail.com>
2023-04-04 23:25:29 +0900

    feat: RepoListViewController에 errorToastMessageLabel 추가 및 이벤트 바인딩

--- MyGitHubTracker/MyGitHubTracker/Source/Presentation/Home/RepoList/RepoListViewController.swift
+++ MyGitHubTracker/MyGitHubTracker/Source/Presentation/Home/RepoList/RepoListViewController.swift
@@ -7,6 +7,7 @@
 
 import RxSwift
 import RxAppState
+import RxCocoa
 import RxDataSources
 import Then
 import SnapKit
@@ -24,6 +25,8 @@ final class RepoListViewController: UIViewController, ViewType {
         $0.rowHeight = UITableView.automaticDimension
         $0.register(RepositoryViewCell.self, forCellReuseIdentifier: RepositoryViewCell.identifier)
     }
+    
+    private let errorToastMessageLabel = ToastLabel()
     
     var viewModel: RepoListViewModel?
     private let disposeBag = DisposeBag()
@@ -50,6 +53,11 @@ final class RepoListViewController: UIViewController, ViewType {
             .map { [RepositorySection(items: $0)] }
             .drive(repositoryTableView.rx.items(dataSource: repositoryTableViewDataSource))
             .disposed(by: disposeBag)
+        
+        output.showErrorMessage
+            .asSignal()
+            .emit(onNext: errorToastMessageLabel.show(message:))
+            .disposed(by: disposeBag)
     }
 }
 
@@ -67,10 +75,17 @@ private extension RepoListViewController {
 private extension RepoListViewController {
     func layoutUI() {
         view.addSubview(repositoryTableView)
+        view.addSubview(errorToastMessageLabel)
         
         repositoryTableView.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
             make.leading.trailing.bottom.equalToSuperview()
+        }
+        
+        errorToastMessageLabel.snp.makeConstraints { make in
+            make.centerX.equalTo(view.snp.centerX)
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).inset(12)
+            make.width.lessThanOrEqualTo(view.snp.width).multipliedBy(0.8)
         }
     }
 }
