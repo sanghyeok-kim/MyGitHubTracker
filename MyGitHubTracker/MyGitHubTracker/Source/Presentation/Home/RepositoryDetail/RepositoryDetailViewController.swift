38c91305470f9b31b8129f0757e9fecff8b828a3
sanghyeok-kim <slsnsi1212@gmail.com>
2023-04-13 10:38:16 +0900

    feat: RepositoryDetailVC, VM 생성 및 바인딩 로직 구현

--- MyGitHubTracker/MyGitHubTracker/Source/Presentation/Home/RepositoryDetail/RepositoryDetailViewController.swift
+++ MyGitHubTracker/MyGitHubTracker/Source/Presentation/Home/RepositoryDetail/RepositoryDetailViewController.swift
@@ -14,11 +14,77 @@ import Then
 
 final class RepositoryDetailViewController: UIViewController, ViewType {
     
+    private lazy var loadingIndicator = UIActivityIndicatorView().then {
+        $0.style = .medium
+        $0.hidesWhenStopped = true
+    }
+    
+    private lazy var userProfileImageView = CircularImageView().then {
+        $0.contentMode = .scaleAspectFit
+        $0.snp.makeConstraints { make in
+            make.width.height.equalTo(30)
+        }
+    }
+    
+    private lazy var userNameLabel = UILabel().then {
+        $0.textColor = .systemGray
+    }
+    
+    private lazy var userProfileStackView = UIStackView().then {
+        $0.addArrangedSubviews([userProfileImageView, userNameLabel])
+        $0.axis = .horizontal
+        $0.spacing = 16
+    }
+    
+    private lazy var titleLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 28, weight: .bold)
+        $0.numberOfLines = .zero
+        $0.lineBreakMode = .byWordWrapping
+    }
+    
+    private lazy var descriptionLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 18)
+        $0.numberOfLines = .zero
+        $0.lineBreakMode = .byWordWrapping
+    }
+    
+    private let stargazerView = StargazerView()
+    
+    private lazy var starsStackView = UIStackView().then {
+        let label = UILabel()
+        label.text = "stars"
+        $0.addArrangedSubviews([stargazerView, label])
+        $0.axis = .horizontal
+        $0.spacing = 4
+        $0.alignment = .center
+        $0.isHidden = true
+    }
+    
+    private lazy var repositoryStackView = UIStackView().then {
+        $0.addArrangedSubviews([titleLabel, descriptionLabel, starsStackView])
+        $0.axis = .vertical
+        $0.spacing = 24
+        $0.distribution = .equalSpacing
+        $0.alignment = .leading
+    }
+    
+    private lazy var starringButton = UIButton().then {
+        var configuration = UIButton.Configuration.filled()
+        configuration.title = "Star"
+        configuration.imagePadding = 5
+        $0.configuration = configuration
+        $0.isHidden = true
+    }
+    
+    @Inject private var imageLoader: ImageLoader
+    
     var viewModel: RepositoryDetailViewModel?
     private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
+        layoutUI()
     }
     
     func bindInput(to viewModel: RepositoryDetailViewModel) {
@@ -27,10 +93,130 @@ final class RepositoryDetailViewController: UIViewController, ViewType {
         rx.viewDidLoad
             .bind(to: input.viewDidLoad)
             .disposed(by: disposeBag)
+        
+        starringButton.rx.tap
+            .bind(to: input.starringButtonDidTap)
+            .disposed(by: disposeBag)
     }
     
     func bindOutput(from viewModel: RepositoryDetailViewModel) {
         let output = viewModel.output
         
+        output.isFetchingData
+            .asDriver()
+            .drive(
+                loadingIndicator.rx.isAnimating,
+                starsStackView.rx.isHidden,
+                starringButton.rx.isHidden
+            )
+            .disposed(by: disposeBag)
+        
+        output.avatarImageURL
+            .compactMap { $0 }
+            .withUnretained(self)
+            .bind { `self`, url in
+                self.userProfileImageView
+                    .setImageWithCaching(from: url, using: self.imageLoader)
+                    .disposed(by: self.disposeBag)
+            }
+            .disposed(by: disposeBag)
+        
+        output.ownerName
+            .asDriver()
+            .drive(userNameLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        output.name
+            .asDriver()
+            .drive(titleLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        output.description
+            .asDriver()
+            .drive(descriptionLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        output.starCount
+            .asDriver()
+            .drive(onNext: stargazerView.set(count:))
+            .disposed(by: disposeBag)
+        
+        output.isStarredByUser
+            .asDriver()
+            .drive(with: self) { `self`, isStarred in
+                self.stargazerView.toggleStarMark(by: isStarred)
+                self.toggleStarringButtonStarMark(by: isStarred)
+            }
+            .disposed(by: disposeBag)
+        
+        output.userDidStar
+            .asSignal()
+            .emit(with: self) { `self`, isStarred in
+                self.stargazerView.toggleStarMark(by: isStarred)
+                self.toggleStarringButtonStarMark(by: isStarred)
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    deinit {
+        CustomLogger.logDeallocation(object: self)
+    }
+}
+
+// MARK: Supporting Methods
+
+private extension RepositoryDetailViewController {
+    func toggleStarringButtonStarMark(by isStarred: Bool) {
+        let imageSymbolConfiguration = UIImage.SymbolConfiguration(font: .systemFont(ofSize: 12))
+        var imageName: String
+        switch isStarred {
+        case true:
+            imageName = "star.fill"
+        case false:
+            imageName = "star"
+        }
+        let image = UIImage(systemName: imageName)?.applyingSymbolConfiguration(imageSymbolConfiguration)
+        starringButton.configuration?.image = image
+    }
+}
+
+// MARK: - UI Configuration
+
+private extension RepositoryDetailViewController {
+    func configureUI() {
+        view.backgroundColor = .systemBackground
+    }
+}
+
+// MARK: - UI Layout
+
+private extension RepositoryDetailViewController {
+    func layoutUI() {
+        view.addSubview(userProfileStackView)
+        view.addSubview(repositoryStackView)
+        view.addSubview(starringButton)
+        view.addSubview(loadingIndicator)
+        
+        userProfileStackView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(16)
+            make.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(16)
+        }
+        
+        repositoryStackView.snp.makeConstraints { make in
+            make.top.equalTo(userProfileStackView.snp.bottom).offset(16)
+            make.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(16)
+            make.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).offset(-16)
+        }
+        
+        starringButton.snp.makeConstraints { make in
+            make.top.equalTo(repositoryStackView.snp.bottom).offset(24)
+            make.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(16)
+            make.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).offset(-16)
+            make.height.equalTo(50)
+        }
+        
+        loadingIndicator.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
     }
 }
--- MyGitHubTracker/MyGitHubTracker/Source/Presentation/Home/RepositoryDetail/RepositoryDetailViewModel.swift
+++ MyGitHubTracker/MyGitHubTracker/Source/Presentation/Home/RepositoryDetail/RepositoryDetailViewModel.swift
@@ -12,19 +12,218 @@ final class RepositoryDetailViewModel: ViewModelType {
     
     struct Input {
         let viewDidLoad = PublishRelay<Void>()
+        let starringButtonDidTap = PublishRelay<Void>()
     }
     
     struct Output {
-        
+        let isFetchingData = BehaviorRelay<Bool>(value: true)
+        let avatarImageURL = BehaviorRelay<URL?>(value: nil)
+        let ownerName = BehaviorRelay<String>(value: "")
+        let name = BehaviorRelay<String>(value: "")
+        let description = BehaviorRelay<String?>(value: nil)
+        let starCount = BehaviorRelay<Int>(value: .zero)
+        let isStarredByUser = BehaviorRelay<Bool>(value: false)
+        let userDidStar = PublishRelay<Bool>()
+        let showErrorMessage = PublishRelay<String>()
     }
     
     let input = Input()
     let output = Output()
     
-    private weak var coordinator: RepoListCoordinator?
+    @Inject private var repositorySearchUseCase: RepositorySearchUseCase
+    @Inject private var starringUseCase: StarringUseCase
+    
+    private weak var coordinator: RepositoryListCoordinator?
     private let disposeBag = DisposeBag()
     
-    init(coordinator: RepoListCoordinator, repository: RepositoryEntity) {
+    init(coordinator: RepositoryListCoordinator, repository: RepositoryEntity) {
         
+        // MARK: - Bind Input: viewDidLoad
+        
+        let fetchedRepositoryDetail = input.viewDidLoad
+            .withUnretained(self)
+            .flatMap { `self`, _ in
+                self.repositorySearchUseCase.fetchRepositoryDetail(
+                    ownerName: repository.ownerName,
+                    repositoryName: repository.name
+                )
+            }
+            .withUnretained(self)
+            .flatMap { `self`, repositoryEntity -> Observable<(RepositoryEntity, Bool)> in
+                self.zipIsStarredByUser(with: repositoryEntity)
+            }
+            .withUnretained(self)
+            .map { `self`, result -> RepositoryEntity in
+                let (repositoryEntity, isStarredByUser) = result
+                return self.updateIsStarredByUser(of: repositoryEntity, bool: isStarredByUser)
+            }
+            .share()
+        
+        fetchedRepositoryDetail
+            .map { _ in false }
+            .distinctUntilChanged()
+            .bind(to: output.isFetchingData)
+            .disposed(by: disposeBag)
+        
+        fetchedRepositoryDetail
+            .map { $0.avatarImageURL }
+            .bind(to: output.avatarImageURL)
+            .disposed(by: disposeBag)
+        
+        fetchedRepositoryDetail
+            .map { $0.ownerName }
+            .bind(to: output.ownerName)
+            .disposed(by: disposeBag)
+        
+        fetchedRepositoryDetail
+            .map { $0.name }
+            .bind(to: output.name)
+            .disposed(by: disposeBag)
+        
+        fetchedRepositoryDetail
+            .map { $0.description }
+            .bind(to: output.description)
+            .disposed(by: disposeBag)
+        
+        fetchedRepositoryDetail
+            .map { $0.stargazersCount }
+            .bind(to: output.starCount)
+            .disposed(by: disposeBag)
+        
+        fetchedRepositoryDetail
+            .map { $0.isStarredByUser }
+            .bind(to: output.isStarredByUser)
+            .disposed(by: disposeBag)
+        
+        // MARK: - Bind Input: starringButtonDidTap
+        
+        let fetchedRepositoryDetailWhenStarringButtonDidTap = input.starringButtonDidTap
+            .withLatestFrom(Observable.combineLatest(output.ownerName, output.name))
+            .withUnretained(self)
+            .flatMapMaterialized { `self`, repositoryInfo -> Observable<RepositoryEntity> in
+                let (owenrName, repositoryName) = repositoryInfo
+                return self.repositorySearchUseCase.fetchRepositoryDetail(ownerName: owenrName, repositoryName: repositoryName)
+            }
+            .share()
+        
+        fetchedRepositoryDetailWhenStarringButtonDidTap
+            .compactMap { $0.error }
+            .doLogError()
+            .toastMeessageMap(to: .failToStarring)
+            .bind(to: output.showErrorMessage)
+            .disposed(by: disposeBag)
+        
+        fetchedRepositoryDetailWhenStarringButtonDidTap
+            .compactMap { $0.element }
+            .map { $0.stargazersCount }
+            .bind(to: output.starCount)
+            .disposed(by: disposeBag)
+        
+        let isStarredByUserWhenStarringButtonDidTap = input.starringButtonDidTap
+            .withLatestFrom(Observable.combineLatest(output.ownerName, output.name))
+            .withUnretained(self) { ($0, $1) }
+            .flatMapMaterialized { `self`, name -> Observable<Bool> in
+                let (userName, repositoryName) = name
+                return self.starringUseCase.checkRepositoryIsStarred(ownerName: userName, repositoryName: repositoryName)
+            }
+            .share()
+        
+        isStarredByUserWhenStarringButtonDidTap
+            .compactMap { $0.error }
+            .doLogError()
+            .toastMeessageMap(to: .failToStarring)
+            .bind(to: output.showErrorMessage)
+            .disposed(by: disposeBag)
+        
+        let repositoryDidStar = isStarredByUserWhenStarringButtonDidTap
+            .compactMap { $0.element }
+            .filter { !$0 }
+            .withLatestFrom(Observable.combineLatest(output.ownerName, output.name))
+            .withUnretained(self)
+            .flatMapCompletableMaterialized { `self`, name in
+                let (userName, repositoryName) = name
+                return self.starringUseCase.starRepository(ownerName: userName, repositoryName: repositoryName)
+            }
+            .share()
+        
+        repositoryDidStar
+            .compactMap { $0.error }
+            .doLogError()
+            .toastMeessageMap(to: .failToStarring)
+            .bind(to: output.showErrorMessage)
+            .disposed(by: disposeBag)
+        
+        repositoryDidStar
+            .compactMap { $0.element }
+            .map { true }
+            .bind(to: output.userDidStar)
+            .disposed(by: disposeBag)
+        
+        let repositoryDidUnstar = isStarredByUserWhenStarringButtonDidTap
+            .compactMap { $0.element }
+            .filter { $0 }
+            .withLatestFrom(Observable.combineLatest(output.ownerName, output.name))
+            .withUnretained(self)
+            .flatMapCompletableMaterialized { `self`, nameInfo in
+                let (ownerName, repositoryName) = nameInfo
+                return self.starringUseCase.unstarRepository(ownerName: ownerName, repositoryName: repositoryName)
+            }
+            .share()
+        
+        repositoryDidUnstar
+            .compactMap { $0.error }
+            .doLogError()
+            .toastMeessageMap(to: .failToStarring)
+            .bind(to: output.showErrorMessage)
+            .disposed(by: disposeBag)
+        
+        repositoryDidUnstar
+            .compactMap { $0.element }
+            .map { false }
+            .bind(to: output.userDidStar)
+            .disposed(by: disposeBag)
+        
+        let starringDidFinished = Observable.merge(repositoryDidStar, repositoryDidUnstar)
+            .withLatestFrom(Observable.combineLatest(output.ownerName, output.name))
+            .withUnretained(self)
+            .flatMapMaterialized { `self`, repositoryInfo -> Observable<RepositoryEntity> in
+                let (owenrName, repositoryName) = repositoryInfo
+                return self.repositorySearchUseCase.fetchRepositoryDetail(ownerName: owenrName, repositoryName: repositoryName)
+            }
+            .share()
+        
+        starringDidFinished
+            .compactMap { $0.element }
+            .map { $0.stargazersCount }
+            .bind(to: output.starCount)
+            .disposed(by: disposeBag)
+        
+        starringDidFinished
+            .compactMap { $0.error }
+            .doLogError()
+            .toastMeessageMap(to: .failToStarring)
+            .bind(to: output.showErrorMessage)
+            .disposed(by: disposeBag)
+    }
+    
+    private func zipIsStarredByUser(with repositoryEntity: RepositoryEntity) -> Observable<(RepositoryEntity, Bool)> {
+        let (ownerName, repositoryName) = (repositoryEntity.ownerName, repositoryEntity.name)
+        let isStarredByUserObservable = self.starringUseCase.checkRepositoryIsStarred(
+            ownerName: ownerName,
+            repositoryName: repositoryName
+        )
+        return Observable.zip(Observable.just(repositoryEntity), isStarredByUserObservable)
+    }
+    
+    private func updateIsStarredByUser(of repositoryEntity: RepositoryEntity, bool: Bool) -> RepositoryEntity {
+        var updatedRepositoryEntity = repositoryEntity
+        updatedRepositoryEntity.isStarredByUser = bool
+        return updatedRepositoryEntity
+    }
+    
+    private func updateStargazerCount(of repositoryEntity: RepositoryEntity, count: Int) -> RepositoryEntity {
+        var updatedRepositoryEntity = repositoryEntity
+        updatedRepositoryEntity.stargazersCount = count
+        return updatedRepositoryEntity
     }
 }
