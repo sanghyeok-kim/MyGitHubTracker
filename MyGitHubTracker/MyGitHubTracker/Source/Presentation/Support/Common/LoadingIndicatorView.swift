78aa8b69e34caf338ba270af91bd42be107eb761
sanghyeok-kim <slsnsi1212@gmail.com>
2023-04-16 14:37:13 +0900

    feat: LoadingIndicatorView 생성 및 구현

---
+++ MyGitHubTracker/MyGitHubTracker/Source/Presentation/Support/Common/LoadingIndicatorView.swift
@@ -0,0 +1,51 @@
+//
+//  LoadingIndicatorView.swift
+//  MyGitHubTracker
+//
+//  Created by 김상혁 on 2023/04/16.
+//
+
+import UIKit
+
+final class LoadingIndicatorView: UIView {
+    
+    lazy var activityIndicator: UIActivityIndicatorView = {
+        let activityIndicator = UIActivityIndicatorView(style: .medium)
+        activityIndicator.hidesWhenStopped = true
+        return activityIndicator
+    }()
+    
+    init() {
+        super.init(frame: .zero)
+        layoutUI()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - External Methods
+
+extension LoadingIndicatorView {
+    func showLoadingIndicatorIfNeeded(_ show: Bool) {
+        if show {
+            activityIndicator.startAnimating()
+        } else {
+            activityIndicator.stopAnimating()
+        }
+    }
+}
+
+// MARK: - UI Configuration
+
+private extension LoadingIndicatorView {
+    func layoutUI() {
+        addSubview(activityIndicator)
+        
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        activityIndicator.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        activityIndicator.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+    }
+}
