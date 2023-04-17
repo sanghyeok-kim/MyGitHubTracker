204f26468fdd92abe066d485958c30f09504dc32
sanghyeok-kim <slsnsi1212@gmail.com>
2023-04-17 20:23:42 +0900

    refactor: asCompletableForStatusCode 커스텀 Rx 연산자 사용하여 코드 개선

--- MyGitHubTracker/MyGitHubTracker/Source/Data/Repository/DefaultStarringRepository.swift
+++ MyGitHubTracker/MyGitHubTracker/Source/Data/Repository/DefaultStarringRepository.swift
@@ -17,46 +17,22 @@ final class DefaultStarringRepository: StarringRepository {
             .fetchStatusCode(
                 endpoint: GitHubAPI.checkRepositoryIsStarredByUser(ownerName: ownerName, repositoryName: repositoryName)
             )
-        .map { $0 == 204 }
+            .map { $0 == 204 }
     }
     
     func starRepository(ownerName: String, repositoryName: String) -> Completable {
-        return performStarring(
-            endpoint: GitHubAPI.starRepository(ownerName: ownerName, repositoryName: repositoryName)
-        )
+        return urlSessionNetworkService
+            .fetchStatusCode(
+                endpoint: GitHubAPI.starRepository(ownerName: ownerName, repositoryName: repositoryName)
+            )
+            .asCompletableForStatusCode(expected: 204)
     }
     
     func unstarRepository(ownerName: String, repositoryName: String) -> Completable {
-        return performStarring(
-            endpoint: GitHubAPI.unstarRepository(ownerName: ownerName, repositoryName: repositoryName)
-        )
-    }
-}
-
-// MARK: - Supporting Methods
-
-private extension DefaultStarringRepository {
-    func performStarring(endpoint: GitHubAPI) -> Completable {
-        return Completable.create { [weak self] observer in
-            guard let self = self else {
-                observer(.error(NetworkError.objectDeallocated))
-                return Disposables.create()
-            }
-            
-            self.urlSessionNetworkService
-                .fetchStatusCode(endpoint: endpoint)
-                .subscribe(onSuccess: { statusCode in
-                    if statusCode == 204 {
-                        observer(.completed)
-                    } else {
-                        observer(.error(NetworkError.invalidStatusCode(code: statusCode)))
-                    }
-                }, onFailure: { error in
-                    observer(.error(error))
-                })
-                .disposed(by: self.disposeBag)
-            
-            return Disposables.create()
-        }
+        return urlSessionNetworkService
+            .fetchStatusCode(
+                endpoint: GitHubAPI.unstarRepository(ownerName: ownerName, repositoryName: repositoryName)
+            )
+            .asCompletableForStatusCode(expected: 204)
     }
 }
