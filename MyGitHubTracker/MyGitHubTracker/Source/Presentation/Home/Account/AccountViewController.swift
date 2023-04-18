d41218e9485296f81a3df735ac97f536d2a8db27
sanghyeok-kim <slsnsi1212@gmail.com>
2023-04-18 10:43:18 +0900

    feat: AccountVC, VM 구현 및 로직 바인딩

--- MyGitHubTracker/MyGitHubTracker/Source/Presentation/Home/Account/AccountViewController.swift
+++ MyGitHubTracker/MyGitHubTracker/Source/Presentation/Home/Account/AccountViewController.swift
@@ -7,8 +7,54 @@
 
 import RxSwift
 import RxAppState
+import RxDataSources
+import Then
+import SnapKit
 
 final class AccountViewController: UIViewController, ViewType {
+    
+    private lazy var avatarImageView = CircularImageView().then {
+        $0.contentMode = .scaleAspectFit
+        $0.snp.makeConstraints { make in
+            make.width.height.equalTo(120)
+        }
+    }
+    
+    private lazy var loginIDLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 16)
+    }
+    
+    private lazy var nameLabel = UILabel().then {
+        $0.font = .boldSystemFont(ofSize: 24)
+    }
+    
+    private lazy var followersCountLabel = UILabel().then {
+        $0.textColor = .gray
+        $0.font = .systemFont(ofSize: 16)
+        $0.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+    }
+    
+    private lazy var followingCountLabel = UILabel().then {
+        $0.textColor = .gray
+        $0.font = .systemFont(ofSize: 16)
+    }
+    
+    private lazy var followersStackView = UIStackView().then {
+        $0.addArrangedSubviews([followersCountLabel, followingCountLabel])
+        $0.axis = .horizontal
+        $0.spacing = 16
+    }
+    
+    private lazy var userInfoStackView = UIStackView().then {
+        $0.addArrangedSubviews([nameLabel, loginIDLabel, followersStackView])
+        $0.axis = .vertical
+        $0.spacing = 8
+        $0.setCustomSpacing(16, after: loginIDLabel)
+    }
+    
+    private lazy var toastMessageLabel = ToastLabel()
+    
+    @Inject private var imageLoader: ImageLoader
     
     var viewModel: AccountViewModel?
     private let disposeBag = DisposeBag()
@@ -30,10 +76,44 @@ final class AccountViewController: UIViewController, ViewType {
     func bindOutput(from viewModel: AccountViewModel) {
         let output = viewModel.output
         
-    }
-    
-    deinit {
-        CustomLogger.logDeallocation(object: self)   
+        output.loginID
+            .asDriver()
+            .drive(loginIDLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        output.avatarImageURL
+            .asDriver()
+            .compactMap { $0 }
+            .drive(avatarImageView.rx.loadImage(using: imageLoader, disposeBag: disposeBag))
+            .disposed(by: disposeBag)
+        
+        output.gitHubURL
+            .asDriver()
+            .compactMap { $0 }
+            .drive()
+            .disposed(by: disposeBag)
+        
+        output.name
+            .asDriver()
+            .drive(nameLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        output.followersCount
+            .asDriver()
+            .map { "\($0) followers" }
+            .drive(followersCountLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        output.followingCount
+            .asDriver()
+            .map { "\($0) following" }
+            .drive(followingCountLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        output.showToastMessage
+            .asSignal()
+            .emit(onNext: toastMessageLabel.show(message:))
+            .disposed(by: disposeBag)
     }
 }
 
@@ -50,6 +130,25 @@ private extension AccountViewController {
 
 private extension AccountViewController {
     func layoutUI() {
+        view.addSubview(avatarImageView)
+        view.addSubview(userInfoStackView)
+        view.addSubview(toastMessageLabel)
         
+        avatarImageView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(18)
+            make.leading.equalTo(view.safeAreaLayoutGuide).offset(16)
+        }
+        
+        userInfoStackView.snp.makeConstraints { make in
+            make.centerY.equalTo(avatarImageView)
+            make.leading.equalTo(avatarImageView.snp.trailing).offset(16)
+            make.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).inset(32)
+        }
+        
+        toastMessageLabel.snp.makeConstraints { make in
+            make.centerX.equalTo(view.snp.centerX)
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).inset(12)
+            make.width.lessThanOrEqualTo(view.snp.width).multipliedBy(0.8)
+        }
     }
 }
--- MyGitHubTracker/MyGitHubTracker/Source/Presentation/Home/Account/AccountViewModel.swift
+++ MyGitHubTracker/MyGitHubTracker/Source/Presentation/Home/Account/AccountViewModel.swift
@@ -7,6 +7,7 @@
 
 import RxSwift
 import RxRelay
+import RxCocoa
 
 final class AccountViewModel: ViewModelType {
     
@@ -15,39 +16,84 @@ final class AccountViewModel: ViewModelType {
     }
     
     struct Output {
-        let fetchedUserInfo = PublishRelay<UserEntity>()
-        let showErrorMessage = PublishRelay<String>()
+        let loginID = BehaviorRelay<String>(value: "")
+        let avatarImageURL = BehaviorRelay<URL?>(value: nil)
+        let gitHubURL = BehaviorRelay<URL?>(value: nil)
+        let followersURL = BehaviorRelay<String>(value: "")
+        let followingURL = BehaviorRelay<String>(value: "")
+        let name = BehaviorRelay<String>(value: "")
+        let followersCount = BehaviorRelay<Int>(value: .zero)
+        let followingCount = BehaviorRelay<Int>(value: .zero)
+        let showToastMessage = PublishRelay<String>()
+    }
+    
+    struct State {
+        let user = PublishRelay<UserEntity>()
     }
     
     let input = Input()
     let output = Output()
+    let state = State()
     
     @Inject private var accountUseCase: AccountUseCase
     
     private weak var coordinator: AccountCoordinator?
     private let disposeBag = DisposeBag()
     
-    init(coordinator: AccountCoordinator) {
+    init(coordinator: AccountCoordinator?) {
         self.coordinator = coordinator
+        
+        // MARK: - Bind Input - viewDidLoad
         
         let userInfo = input.viewDidLoad
             .withUnretained(self)
-            .flatMapLatest { `self`, _ in
+            .flatMapMaterialized { `self`, _ in
                 self.accountUseCase.fetchUserInfo()
             }
-            .materialize()
             .share()
         
         userInfo
             .compactMap { $0.element }
-            .bind(to: output.fetchedUserInfo)
+            .bind(to: state.user)
             .disposed(by: disposeBag)
         
         userInfo
             .compactMap { $0.error }
-            .catchAndLogError(logType: .error)
+            .doLogError(logType: .error)
             .toastMeessageMap(to: .failToFetchUserInformation)
-            .bind(to: output.showErrorMessage)
+            .bind(to: output.showToastMessage)
+            .disposed(by: disposeBag)
+        
+        // MARK: - Bind State - user
+        
+        state.user
+            .map { $0.loginID }
+            .bind(to: output.loginID)
+            .disposed(by: disposeBag)
+        
+        state.user
+            .map { $0.avatarImageURL }
+            .bind(to: output.avatarImageURL)
+            .disposed(by: disposeBag)
+        
+        state.user
+            .map { $0.gitHubURL }
+            .bind(to: output.gitHubURL)
+            .disposed(by: disposeBag)
+        
+        state.user
+            .map { $0.name }
+            .bind(to: output.name)
+            .disposed(by: disposeBag)
+        
+        state.user
+            .map { $0.followersCount }
+            .bind(to: output.followersCount)
+            .disposed(by: disposeBag)
+        
+        state.user
+            .map { $0.followingCount }
+            .bind(to: output.followingCount)
             .disposed(by: disposeBag)
     }
 }
