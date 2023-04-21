e425337d48f3acd6010d11898047bdab94f93d98
sanghyeok-kim <slsnsi1212@gmail.com>
2023-04-21 23:24:16 +0900

    feat: Rx+prepareForReuse Reactive 확장자 구현

---
+++ MyGitHubTracker/MyGitHubTracker/Source/Common/Extension/Rx+prepareForReuse.swift
@@ -0,0 +1,29 @@
+//
+//  Rx+UITableViewCell.swift
+//  MyGitHubTracker
+//
+//  Created by 김상혁 on 2023/04/21.
+//
+
+import RxSwift
+
+extension Reactive where Base: UICollectionViewCell {
+    var prepareForReuse: Observable<Void> {
+        let targetSelector = #selector(Base.prepareForReuse)
+        return sentMessage(targetSelector).map { _ in }
+    }
+}
+
+extension Reactive where Base: UITableViewCell {
+    var prepareForReuse: Observable<Void> {
+        let targetSelector = #selector(Base.prepareForReuse)
+        return sentMessage(targetSelector).map { _ in }
+    }
+}
+
+extension Reactive where Base: UITableViewCell {
+    var cellDidLoad: Observable<Void> {
+        let targetSelector = #selector(Base.init(style:reuseIdentifier:))
+        return sentMessage(targetSelector).map { _ in }
+    }
+}
